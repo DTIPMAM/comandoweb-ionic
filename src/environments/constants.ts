eaf4327e9d1078b9a5521d97e393a23a56497668
AdemarCastro <2022002444@ifam.edu.br>
2024-01-15 09:23:32 -0400

    [FEAT] - Adiciona 'constants.ts'

---
+++ src/environments/constants.ts
@@ -0,0 +1,5 @@
+export const constants = {
+  appName: 'Sistema ComandoWeb da Polícia Militar do Amazonas',
+  appinitials: "ComandoWeb",
+  copyright: "Copyright © 1835-2023 Polícia Militar do Estado do Amazonas. Todo o conteúdo deste site é de uso exclusivo da Polícia Militar. Proibida reprodução ou utilização a qualquer título, sob as penas da lei. Diretoria de Tecnologia da Informação - DTI/PMAM"
+}
